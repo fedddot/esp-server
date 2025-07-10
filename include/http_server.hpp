7f67a0263b7153c31ac57cc738ffb7978d967ba4
fedddot <idanrommm@gmail.com>
2025-07-10 14:46:44 +0000

    reconfigured to listen on AP

--- include/http_server.hpp
+++ include/http_server.hpp
@@ -5,15 +5,50 @@
 
 #include "esp_http_server.h"
 #include "http_parser.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
 
 namespace mcu_server {
     class HttpServer {
     public:
         HttpServer() {
+            // Initialize NVS
+            esp_err_t ret = nvs_flash_init();
+            if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+                ESP_ERROR_CHECK(nvs_flash_erase());
+                ESP_ERROR_CHECK(nvs_flash_init());
+            }
+
+            // Initialize TCP/IP and event loop
+            ESP_ERROR_CHECK(esp_netif_init());
+            ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+            // Create default WiFi AP
+            esp_netif_create_default_wifi_ap();
+
+            wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+            ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+            wifi_config_t ap_config = {};
+            strcpy((char*)ap_config.ap.ssid, "ESP32-AP");
+            ap_config.ap.ssid_len = strlen("ESP32-AP");
+            strcpy((char*)ap_config.ap.password, "12345678");
+            ap_config.ap.max_connection = 4;
+            ap_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;
+            if (strlen("12345678") == 0) {
+                ap_config.ap.authmode = WIFI_AUTH_OPEN;
+            }
+
+            ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+            ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &ap_config));
+            ESP_ERROR_CHECK(esp_wifi_start());
+
+            // Start HTTP server
             httpd_config_t config = HTTPD_DEFAULT_CONFIG();
             httpd_handle_t server = nullptr;
             httpd_uri_t post_handler {
-                .uri = "post_route",
+                .uri = "/post_route",
                 .method = HTTP_POST,
                 .handler = HttpServer::post_handler,
                 .user_ctx = this
