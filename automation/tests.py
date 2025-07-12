ead5b5bd32cd786f73d73bc4cc1be92af3d03a9c
fedddot <idanrommm@gmail.com>
2025-07-12 08:22:00 +0000

    test implemented

---
+++ automation/tests.py
@@ -0,0 +1,25 @@
+import requests
+from service_api_pb2 import ThermostatApiRequest, ThermostatApiResponse, RequestType, StatusCode
+
+def send_start_request(service_uri, temp_setup, time_resolution_ms):
+    # build ThermostatApiRequest protobuf message (see service_api.proto for reference) according to the provided parameters
+    request_msg = ThermostatApiRequest()
+    request_msg.request_type = RequestType.START
+    request_msg.set_temperature = temp_setup
+    request_msg.time_resolution_ms = time_resolution_ms
+
+    serialized = request_msg.SerializeToString()
+    headers = {'Content-Type': 'application/octet-stream'}
+    response = requests.post(service_uri, data=serialized, headers=headers)
+
+    response_msg = ThermostatApiResponse()
+    response_msg.ParseFromString(response.content)
+    return response_msg
+
+# test the function with service_uri=http://192.168.1.129:5555/test_route
+if __name__ == "__main__":
+    service_uri = "http://192.168.1.129:5555/test_route"
+    temp_setup = 22.5  # example temperature setup
+    time_resolution_ms = 1000  # example time resolution
+    response = send_start_request(service_uri, temp_setup, time_resolution_ms)
+    print("Received response:", response)
