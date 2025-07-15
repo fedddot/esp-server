0c8dd9ce5c668e98f96025012155f46941571c66
fedddot <idanrommm@gmail.com>
2025-07-15 18:56:32 +0000

    freertos and nvs deleted

--- include/wifi_station_guard.hpp
+++ include/wifi_station_guard.hpp
@@ -15,11 +15,6 @@ namespace mcu_server {
             if (s_reference_count) {
                 throw std::runtime_error("WifiStationGuard is already initialized");
             }
-            esp_err_t ret = nvs_flash_init();
-            if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-                ESP_ERROR_CHECK(nvs_flash_erase());
-                ESP_ERROR_CHECK(nvs_flash_init());
-            }
             ESP_ERROR_CHECK(esp_netif_init());
             ESP_ERROR_CHECK(esp_event_loop_create_default());
             esp_netif_create_default_wifi_sta();
